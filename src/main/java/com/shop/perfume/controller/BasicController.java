8cd9878044f7265de75c4f67c995645d18b2ebb8
echang1 <echang1@ftdm.com.tw>
2021-02-21 15:37:32 +0800

    first commit

---
+++ src/main/java/com/shop/perfume/controller/BasicController.java
@@ -0,0 +1,19 @@
+package com.shop.perfume.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+  @GetMapping("/")
+  public String home() {
+    return "Hello World!";
+  }
+
+  @GetMapping("/api/test")
+  public String apiTest() {
+    System.out.println("HERE");
+    return "Perfume Shop";
+  }
+}
